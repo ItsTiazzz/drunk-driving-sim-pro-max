e42b51b604d0b128c7c702bc73436c85ab297a62
ItsTiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2026-02-16 18:44:20 +0100

    Auto verbeteringen

--- README.md
+++ README.md
@@ -3,11 +3,7 @@ Small driving game in which you drive while drunk.
 ## Attributions
 ### Models
 - Beer by "Poly by Google" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/0o4ac2O90Hu)
-- Van by "Poly by Google" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/aT_24cDaW1a)
-- Convertible by "Poly by Google" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/dggOiBLYyuR)
 - Truck by "jeremy" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/cPVFA5uTr9l)
-- Van by "jeremy" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/9hUUD2DWk1v)
-- Golf cart by "Michael Ruddy" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/0ZN5hIrQCUh)
 - Family Room by "Danni Bittman" [[CC-BY](https://creativecommons.org/licenses/by/3.0/)] via Poly Pizza (https://poly.pizza/m/7hs7nUg9j0N)
 
 ## License
--- scenes/car_player.gd
+++ scenes/car_player.gd
@@ -6,6 +6,10 @@ const ENGINE_POWER = 100
 @onready var camera_pivot: Node3D = $CameraPivot
 @onready var camera_3d: Camera3D = $CameraPivot/Camera3D
 @onready var reverse_camera_3d: Camera3D = $CameraPivot/ReverseCamera3D
+@onready var player: CharacterBody3D = $"../Player"
+@onready var player_camera: Camera3D = $"../Player/Head/Camera"
+@onready var body: MeshInstance3D = $Body
+@onready var this: VehicleBody3D = $"."
 
 var look_direction
 var driving = false
@@ -21,25 +25,35 @@ func interact() -> void:
 
 func enter() -> void:
 	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	player.visible = false
+	player.add_collision_exception_with(this)
 	driving = true
 
 func leave() -> void:
 	driving = false
+	player.global_position = global_position
+	player.global_position.x = player.global_position.x + 5
+	player.visible = true
+	player.remove_collision_exception_with(this)
+	player_camera.current = true
 
 func get_prompt() -> String:
-	return "Druk E om in te stappen" if driving else "Druk E om uit te stappen"
+	return "Druk E om uit te stappen" if driving else "Druk E om in te stappen"
 
 func _physics_process(delta: float) -> void:
 	if not driving:
 		return
-	steering = move_toward(steering, Input.get_axis("ui_right", "ui_left") * MAX_STEER, delta * 2.5)
-	engine_force = Input.get_axis("ui_down", "ui_up") * ENGINE_POWER
+	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 2.5)
+	engine_force = Input.get_axis("backward", "forward") * ENGINE_POWER
 	camera_pivot.global_position = camera_pivot.global_position.lerp(global_position, delta * 20.0)
 	camera_pivot.transform = camera_pivot.transform.interpolate_with(transform, delta * 5.0)
 	look_direction = look_direction.lerp(global_position + linear_velocity, delta * 5.0)
 	_check_camera_switch()
 	camera_3d.look_at(look_direction)
 	reverse_camera_3d.look_at(look_direction)
+	
+	if Input.is_action_just_released("exit"):
+		leave()
 	
 func _check_camera_switch():
 	if linear_velocity.dot(transform.basis.z) > -0.05:
